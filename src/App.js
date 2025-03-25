7a7f16543f31f955396c960d3a86bb3094d292ff
qiuchp@126.com <qiuchp@126.com>
2025-03-25 17:07:47 +0800

    完善sepolia测试网络合约调用

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ import FundMeABI from './FundMeABI.json';
 function App() {
 
   const [web3, setWeb3] = useState(null);
-  const [errorMessage, setErrorMessage] = useState('');
-  const [status,setStatus] = useState('');
+  const [message, setMessage] = useState('');
   const [walletAddress, setWalletAddress] = useState('');
   const [fundMeContract, setFundMeContract] = useState(null);
   const [fundAmount,setFundAmount] = useState('');
   const [withdrawAmount,setWithdrawAmount] = useState('');
 
-  const ContractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+  // const ContractAddress = "0xF1Bb350D88b7C61ffdE94cd638b784DC08DA0b85";
+  const ContractAddress = "0x3DD817E767a6d762eF5d13bAAb0e7Dc784Ce7a07";
 
   const fundInputAmount = (e) => {
     setFundAmount(e.target.value);
@@ -30,16 +30,16 @@ function App() {
     try {
         // 检查是否安装了钱包扩展程序
         if (!window.ethereum) {
-            setErrorMessage('请安装钱包扩展程序（如 MetaMask）');
+            setMessage('请安装钱包扩展程序（如 MetaMask）');
             return;
         }
-        setStatus('连接中');
+        setMessage('连接中');
         // 请求用户授权访问钱包
         const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
         if (accounts.length > 0) {
             setWalletAddress(accounts[0]);
         } else {
-            setErrorMessage('未获取到钱包地址');
+            setMessage('未获取到钱包地址');
             return;
         }
         // 创建 Web3 实例
@@ -48,11 +48,9 @@ function App() {
         // 创建合约实例
         const fundMeContract = new web3.eth.Contract(FundMeABI, ContractAddress);
         setFundMeContract(fundMeContract);
-        setStatus('连接成功');
-        // 清除错误消息
-        setErrorMessage('');
+        setMessage('连接成功');
     } catch (error) {
-        setErrorMessage(`连接钱包失败: ${error.message}`);
+        setMessage(`连接钱包失败: ${error.message}`);
     }
   };
 
@@ -60,31 +58,30 @@ function App() {
   const queryBalance = async () => {
     if (web3 && walletAddress && fundMeContract) {
       try {
-          setStatus('查询中');
+          setMessage('查询中');
           const result = await fundMeContract.methods.balanceOf().call({ from: walletAddress });
-          const fmtBalance = parseFloat(result).toFixed(2);
-          setStatus('捐赠金额：'+fmtBalance + ' ETH');
-          setErrorMessage('');
+          console.log('余额：'+result);
+          const fmtBalance = web3.utils.fromWei(result, 'ether');
+          setMessage('捐赠金额：'+fmtBalance + ' ETH');
       } catch (error) {
         console.error("查询余额时出错:", error);
-        setErrorMessage(`查询余额失败: ${error.message}`);
+        setMessage("查询余额失败");
       }
     } else {
-        setErrorMessage('请先连接钱包');
+      setMessage('请先连接钱包');
     }
   }
   // 捐赠
   const fund = async () => {
     if (web3 && walletAddress && fundMeContract) {
       try {
-          setStatus('捐赠中');
+          setMessage('捐赠中');
           const result = await fundMeContract.methods.fund().send({ from: walletAddress,value: web3.utils.toWei(fundAmount, 'ether')});
           console.log('捐赠结果：'+result);
-          setStatus('捐赠成功');
-          setErrorMessage('');
+          setMessage('捐赠成功');
       } catch (error) {
         console.error("捐赠时出错:", error);
-        setErrorMessage(`捐赠失败: ${error.message}`);
+        setMessage("捐赠失败");
       }
     }
   }
@@ -93,14 +90,13 @@ function App() {
     console.log('提现金额：'+withdrawAmount);
     if (web3 && walletAddress && fundMeContract) {
       try {
-          setStatus('提现中');
+          setMessage('提现中');
           const result = await fundMeContract.methods.withdraw(web3.utils.toWei(withdrawAmount, 'ether')).send({ from: walletAddress});
-          console.log('提现结果：'+result);
-          setStatus('提现成功');
-          setErrorMessage('');
+          console.log('提现结果：'+ result);
+          setMessage('提现成功');
       } catch (error) {
         console.error("提现时出错:", error);
-        setErrorMessage(`提现失败: ${error.message}`);
+        setMessage("提现失败");
       }
     }
   }
@@ -109,7 +105,9 @@ function App() {
     <div className="App">
       <div className="form-container">
         <h2 className="form-title">请给“我”捐赠</h2>
-        <h3>{errorMessage}</h3>
+        <div>【测试网络：Sepolia】<br/>
+        【合约地址：0x3DD817E767a6d762eF5d13bAAb0e7Dc784Ce7a07】</div>
+        <h4>【当前账户：{walletAddress}】</h4>
         <div className="button-container">
           <button onClick={connectWallet}>连接钱包</button>
           <button onClick={queryBalance}>查询捐赠金额</button>
@@ -122,8 +120,7 @@ function App() {
           <input type="text" placeholder="输入提现金额" onChange={withdrawInputAmount}/>
           <button onClick={withdraw}>提现</button>
         </div>
-        <p className="status">{status}</p>
-        <p className="status">当前账户：{walletAddress}</p>
+        <p className="status">{message}</p>
       </div>
     </div>
   );
